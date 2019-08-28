8f18919133d204514d5a6b97b34fd8a114f24eaa
Tomas Moran <moran.tomasgabriel@gmail.com>
2019-08-27 21:05:26 -0300

    First Commit

---
+++ WeatherTest/Extras/Clases/GradientView.swift
@@ -0,0 +1,43 @@
+//
+//  GradientView.swift
+//  WeatherTest
+//
+//  Created by Tomas Moran on 27/08/2019.
+//  Copyright © 2019 Tomas Moran. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+@IBDesignable
+class GradientView: UIView {
+    
+    @IBInspectable var FirstColor: UIColor = UIColor.clear {
+        didSet {
+            updateView()
+        }
+    }
+    
+    @IBInspectable var SecondColor: UIColor = UIColor.clear {
+        didSet {
+            updateView()
+        }
+    }
+    
+    override class var layerClass: AnyClass {
+        get {
+            return CAGradientLayer.self
+        }
+    }
+    
+    func updateView() {
+        let layer: CAGradientLayer = self.layer as! CAGradientLayer
+        layer.colors = [FirstColor.cgColor, SecondColor.cgColor]
+        
+        layer.locations = [0.7]
+        
+        layer.startPoint = CGPoint(x: 0, y: 0)
+        layer.endPoint = CGPoint(x: 0, y: 1)
+    }
+    
+}
